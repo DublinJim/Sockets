4436f52e8dda001d9b8f720a020ca38a1e4b14fe
James Keogh <jkeogh@adga.ca>
2022-07-21 15:03:44 -0400

    finished Client class

--- src/com/jamesMkeogh/Client.java
+++ src/com/jamesMkeogh/Client.java
@@ -5,15 +5,15 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Main {
+public class Client {
 
     public static void main(String[] args) throws IOException {
         /*  String ip = "192.168.180.118";*/
-        String locahHost = "localhost";
+        String localhost = "localhost";
         String msg ="outbound";
         int port; //0 -1023 to 65535
         port = 9489;
-        Socket socket = new Socket(locahHost, port);
+        Socket socket = new Socket(localhost, port);
         OutputStreamWriter os = new OutputStreamWriter(socket.getOutputStream());
         PrintWriter out = new PrintWriter(os);
 

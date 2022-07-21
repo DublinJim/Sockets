943745406e07b925ba36abd213c9035cfeea838c
James Keogh <jkeogh@adga.ca>
2022-07-21 13:59:51 -0400

    init commit

---
+++ src/com/jamesMkeogh/Main.java
@@ -0,0 +1,15 @@
+package com.jamesMkeogh;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int port; //0 -1023 to 65535
+        port=1;
+        Socket socket= new Socket("localhost",port);
+	// write your code here
+    }
+}

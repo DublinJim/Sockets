48b854851b9f56a89e98b9fcd4d35109a21de153
James Keogh <jkeogh@adga.ca>
2022-07-21 14:49:48 -0400

    client app

--- src/com/jamesMkeogh/Main.java
+++ src/com/jamesMkeogh/Main.java
@@ -1,15 +1,22 @@
 package com.jamesMkeogh;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        /*  String ip = "192.168.180.118";*/
+        String locahHost = "localhost";
+        String msg ="outbound";
         int port; //0 -1023 to 65535
-        port=1;
-        Socket socket= new Socket("localhost",port);
-	// write your code here
+        port = 9489;
+        Socket socket = new Socket(locahHost, port);
+        OutputStreamWriter os = new OutputStreamWriter(socket.getOutputStream());
+        PrintWriter out = new PrintWriter(os);
+
+        // write your code here
     }
 }

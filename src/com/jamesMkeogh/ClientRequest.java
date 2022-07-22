4ae83928ffd55f5ba5053579f0a396a5bacaeabd
James Keogh <jkeogh@adga.ca>
2022-07-22 15:49:38 -0400

    adds a while loop

--- src/com/jamesMkeogh/ClientRequest.java
+++ src/com/jamesMkeogh/ClientRequest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import javax.swing.JOptionPane;
 
 public class ClientRequest {
 
@@ -16,18 +15,25 @@ public class ClientRequest {
         Socket socket = new Socket(SERVER_IP, SERVER_PORT);
 
         BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader keyboard = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+        BufferedReader keyboard = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
 
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+        while (true) {
+            System.out.println("> ");
+            String command = keyboard.readLine();
+            out.println(command);
+            if (command == "quit")
+                break;
+            String serverResponse = input.readLine();
+            System.out.println(serverResponse);
+        }
 
-
-        String serverResponse = input.readLine();
-        JOptionPane.showMessageDialog(null, serverResponse);
-        System.out.println(serverResponse);
         socket.close();
-
         System.exit(0);
+
+
     }
 
 }

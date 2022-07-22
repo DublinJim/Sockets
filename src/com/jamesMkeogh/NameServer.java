f852ffdb7671b64bf974f01f31c2688027a15305
James Keogh <jkeogh@adga.ca>
2022-07-22 15:18:43 -0400

    name server made

--- src/com/jamesMkeogh/NameServer.java
+++ src/com/jamesMkeogh/NameServer.java
@@ -1,14 +1,31 @@
 package com.jamesMkeogh;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class NameServer {
 
     private static final int PORT = 9090;
     private static final String[] names = {"Joe", "Mick", "Bob", "Paddy", "Mack", "Pippy"};
     private static final String[] desc = {"fat", "ugly", "fit", "smart", "happy", "stupid"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println(getRandomName());
+        ServerSocket listener = new ServerSocket(PORT);
+        System.out.println("[SERVER} is waiting for client connection...");
+        Socket client = listener.accept();
+        System.out.println("[SERVER} is connected to client.");
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+
+        out.println(getRandomName());
+
+        System.out.println("[SERVER} Sent data");
+
+        client.close();
+        listener.close();
 
     }
 
